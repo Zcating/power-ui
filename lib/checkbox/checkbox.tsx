122c72b7d957f00e5de1f6184d63b4b02cf0ce16
zcating <zcating@qq.com>
2020-11-22 10:21:05 +0800

    feat: add checkbox

--- lib/checkbox/checkbox.tsx
+++ lib/checkbox/checkbox.tsx
@@ -75,7 +75,6 @@ export const Checkbox = defineComponent({
             onBlur={handleBlur}
           />,
           <input
-            v-else
             class="el-checkbox-button__original"
             type="checkbox"
             name={props.name}
